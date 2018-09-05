6f8d355ca79fdf30618ac24071738187e64080bd
John Baker <johnandrewbaker88@gmail.com>
2018-09-05 10:37:35 -0700

    Inital Project Commit

---
+++ SchemaCompare.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.SqlServer.Dac.Compare;
+
+namespace SchemaCompare.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var target = new SchemaCompareDatabaseEndpoint(@"Data Source=localhost;Initial Catalog=Evolution;Integrated Security=True;Connect Timeout=30");
+            var source = new SchemaCompareDatabaseEndpoint(@"Data Source=localhost;Initial Catalog=qaDrmDevTrunk;Integrated Security=True;Connect Timeout=30");
+            var compare = new SchemaComparison(source, target);
+
+            var compRes = compare.Compare();
+            var script = compRes.GenerateScript("Evolution");
+            
+
+            Console.Read();
+        }
+    }
+}
---
+++ SchemaCompare.Core/SchemaCompareWrapper.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.SqlServer.Dac.Compare;
+using System;
+using System.Data.SqlClient;
+
+namespace SchemaCompare.Core
+{
+    public class SchemaCompareWrapper
+    {
+        private SchemaCompareDacpacEndpoint _sourceDacPac;
+        private SchemaCompareDatabaseEndpoint _enpointDb;
+        private SchemaComparisonResult _compareResult;
+        private string _dacPacFileLocation;
+        private string _enpointConnectionString;
+
+        public SchemaCompareWrapper(string dacPacFileLocation, string enpointConnectionString)
+        {
+            _dacPacFileLocation = dacPacFileLocation;
+            _enpointConnectionString = enpointConnectionString;
+            _sourceDacPac = new SchemaCompareDacpacEndpoint(dacPacFileLocation);
+            _enpointDb = new SchemaCompareDatabaseEndpoint(enpointConnectionString);
+        }
+
+        public SchemaComparisonResult Compare(string compareFileLocation = null)
+        {
+            var compare = new SchemaComparison(_sourceDacPac, _enpointDb);
+            compare.Options.BlockOnPossibleDataLoss = false;
+             
+            if (compareFileLocation != null)
+                compare.SaveToFile(compareFileLocation);
+
+            compare = new SchemaComparison(compareFileLocation);
+
+            _compareResult = compare.Compare();
+
+            return _compareResult;
+        }
+
+        public SchemaComparePublishResult Publish()
+        {
+            return _compareResult.PublishChangesToTarget();
+        }
+
+        public SchemaCompareScriptGenerationResult Script()
+        {
+            var dbName = new SqlConnection(_enpointConnectionString).Database;
+
+            return _compareResult.GenerateScript(dbName);
+        }
+    }
+}
